6b22f1d0855a5eed5335f605ff2938447e0d1021
Nick V <nicholas.vanorden83@gmail.com>
2025-11-07 23:59:23 -0700

    Add all project files (#1)

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"goth-ffb-players/handlers"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	// Routes
+	mux.HandleFunc("GET /", handlers.PlayerList)
+	mux.HandleFunc("GET /players/{id}", handlers.PlayerDetail)
+	mux.HandleFunc("GET /search", handlers.SearchPlayers)
+
+	log.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
